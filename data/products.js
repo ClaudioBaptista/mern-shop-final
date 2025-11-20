6862fd74e321b994631e0d99aa1bfaef76dc8784
Claudio Baptista <claudioyuribaptista@icloud.com>
2025-11-19 21:44:12 -0300

    Atualizando lista de produtos

--- data/products.js
+++ data/products.js
@@ -1,9 +1,9 @@
 const products = [
   {
     name: 'Airpods Wireless Bluetooth Headphones',
-    image: '/images/airpods.jpg',
+    image: 'https://raw.githubusercontent.com/bradtraversy/proshop_mern/master/frontend/public/images/airpods.jpg',
     description:
-      'Tecnologia Bluetooth permite conectá-lo a dispositivos compatíveis sem fio. Áudio de alta qualidade AAC oferece experiência de audição imersiva. Microfone embutido permite atender chamadas enquanto trabalha.',
+      'Tecnologia Bluetooth permite conectá-lo a dispositivos compatíveis sem fio. Áudio de alta qualidade AAC oferece experiência de audição imersiva.',
     brand: 'Apple',
     category: 'Electronics',
     price: 89.99,
@@ -13,9 +13,9 @@ const products = [
   },
   {
     name: 'iPhone 11 Pro 256GB Memory',
-    image: '/images/phone.jpg',
+    image: 'https://raw.githubusercontent.com/bradtraversy/proshop_mern/master/frontend/public/images/phone.jpg',
     description:
-      'Apresentando o iPhone 11 Pro. Um sistema de câmera tripla transformador que adiciona toneladas de capacidade sem complexidade. Um salto sem precedentes na duração da bateria.',
+      'Apresentando o iPhone 11 Pro. Um sistema de câmera tripla transformador que adiciona toneladas de capacidade sem complexidade.',
     brand: 'Apple',
     category: 'Electronics',
     price: 599.99,
@@ -25,9 +25,9 @@ const products = [
   },
   {
     name: 'Cannon EOS 80D DSLR Camera',
-    image: '/images/camera.jpg',
+    image: 'https://raw.githubusercontent.com/bradtraversy/proshop_mern/master/frontend/public/images/camera.jpg',
     description:
-      'Caracterizada por especificações de imagem versáteis, a Canon EOS 80D esclarece-se ainda mais usando um par de sistemas de foco robustos e um design intuitivo.',
+      'Caracterizada por especificações de imagem versáteis, a Canon EOS 80D esclarece-se ainda mais usando um par de sistemas de foco robustos.',
     brand: 'Cannon',
     category: 'Electronics',
     price: 929.99,
@@ -37,7 +37,7 @@ const products = [
   },
   {
     name: 'Sony Playstation 5',
-    image: '/images/playstation.jpg',
+    image: 'https://raw.githubusercontent.com/bradtraversy/proshop_mern/master/frontend/public/images/playstation.jpg',
     description:
       'O melhor console de jogos para o entretenimento da sua casa. Jogue em 4k com gráficos de última geração.',
     brand: 'Sony',
@@ -45,30 +45,6 @@ const products = [
     price: 399.99,
     countInStock: 11,
     rating: 5,
-    numReviews: 12,
-  },
-  {
-    name: 'Logitech G-Series Gaming Mouse',
-    image: '/images/mouse.jpg',
-    description:
-      'Tenha uma melhor experiência com seus jogos. O mouse LIGHTSYNC RGB possui 6 botões programáveis e desempenho de nível de competição.',
-    brand: 'Logitech',
-    category: 'Electronics',
-    price: 49.99,
-    countInStock: 7,
-    rating: 3.5,
-    numReviews: 10,
-  },
-  {
-    name: 'Amazon Echo Dot 3rd Generation',
-    image: '/images/alexa.jpg',
-    description:
-      'Conheça o Echo Dot - Nosso alto-falante inteligente mais popular com um design de tecido. É o nosso alto-falante inteligente mais compacto que se encaixa perfeitamente em pequenos espaços.',
-    brand: 'Amazon',
-    category: 'Electronics',
-    price: 29.99,
-    countInStock: 0,
-    rating: 4,
     numReviews: 12,
   },
 ];
